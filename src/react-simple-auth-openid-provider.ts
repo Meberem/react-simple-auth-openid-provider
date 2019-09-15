05c13df196872fb39f2f868aa5fa32a4f1064d92
Matt Trussler <meberem@gmail.com>
2019-09-15 17:13:22 +0200

    fix for all typings

---
+++ src/react-simple-auth-openid-provider.ts
@@ -0,0 +1,6 @@
+import { OpenIdConfiguration } from "./OpenIdConfiguration";
+import { OpenIdProvider } from "./OpenIdProvider";
+import { SecurityOptions } from "./SecurityOptions";
+import { Session } from "./Session";
+
+export { OpenIdConfiguration, OpenIdProvider, SecurityOptions, Session };
